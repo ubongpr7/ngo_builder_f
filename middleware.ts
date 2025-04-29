e1aac77cb549a599c8f3268e5520d32d05712b76
UbongPr7 <ubongpr7@gmail.com>
2025-04-29 17:20:23 +0100

    feat: add '/accounts/verify' to public paths in middleware

--- middleware.ts
+++ middleware.ts
@@ -14,6 +14,7 @@ export function middleware(request: NextRequest) {
     "/forgot-password",
     "/reset-password",
     "/verify-email",
+    "/accounts/verify",
 
     // About section
     "/about",
