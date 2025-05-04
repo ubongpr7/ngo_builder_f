32cae994ada3b2ddc2d9683503c0b990182a3755
shahnawaz <shahnawazgull2166@gmail.com>
2025-05-04 12:41:44 +0500

    responsiveness

--- components/layout/AuthenticatedFooter.tsx
+++ components/layout/AuthenticatedFooter.tsx
@@ -6,13 +6,13 @@ export default function AuthenticatedFooter() {
   const currentYear = new Date().getFullYear()
 
   return (
-    <footer className="bg-[#040b13] border-t py-20">
+    <footer className="bg-[#040b13] border-t py-10 sm:py-20">
       <div className="container mx-auto px-4">
-        <div className="flex flex-col md:flex-row justify-between items-center">
+        <div className="flex flex-col items-center text-center md:flex-row md:justify-between md:items-center md:text-left">
           <div className="text-sm text-gray-300">
             © {currentYear} Destiny Builders Empowerment Foundation. All rights reserved.
           </div>
-          <div className="flex space-x-6 mt-4 md:mt-0">
+          <div className="flex flex-col space-y-2 sm:flex-row sm:space-y-0 sm:space-x-6 mt-4 md:mt-0 justify-center">
             <Link href="/dashboard/help" className="text-sm text-gray-300 hover:text-green-700 hover:underline">
               Help Center
             </Link>
