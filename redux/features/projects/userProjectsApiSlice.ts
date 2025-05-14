c056c4e3eeb4cd8c47d33052dd89cf3dfde2edbf
UbongPr7 <ubongpr7@gmail.com>
2025-05-14 12:28:53 +0100

    my projects

--- redux/features/projects/userProjectsApiSlice.ts
+++ redux/features/projects/userProjectsApiSlice.ts
@@ -3,19 +3,19 @@ import type { Project } from "@/types/project"
 
 export interface UserProjectRole extends Project {
   user_role: "manager" | "official" | "creator" | "team_member"
-  
+
 }
 
 export const userProjectsApiSlice = apiSlice.injectEndpoints({
   endpoints: (builder) => ({
     getUserProjects: builder.query<UserProjectRole[], string>({
-      query: (filters = "") => `/api/user-projects/${filters ? `?${filters}` : ""}`,
+      query: (filters = "") => `/project_api/user-projects/${filters ? `?${filters}` : ""}`,
       
     }),
 
     // Get projects by role
     getProjectsByRole: builder.query<UserProjectRole[], string>({
-      query: (role) => `/api/user-projects/?role=${role}`,
+      query: (role) => `/project_api/user-projects/?role=${role}`,
     }),
   }),
 })
