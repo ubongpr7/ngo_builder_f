f19bc2c30d49013ed43234566509d20fdda0c320
UbongPr7 <ubongpr7@gmail.com>
2025-05-17 08:03:29 +0100

    my projects

--- components/dashboard/project-ranking.tsx
+++ components/dashboard/project-ranking.tsx
@@ -17,6 +17,7 @@ interface ProjectRankingProps {
 
 export function ProjectRanking({ projects, isLoading = false, onRefresh }: ProjectRankingProps) {
   const rankedProjects = rankProjectsByMilestones(projects)
+  console.log("Ranked Projects", rankedProjects)
 
   return (
     <Card>
