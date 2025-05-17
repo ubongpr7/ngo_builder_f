08e73c4b29e2983aa528b4ac9477c8ddeb5656e7
UbongPr7 <ubongpr7@gmail.com>
2025-05-17 06:26:57 +0100

    my projects

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,6 @@ import type React from "react"
 import type { Metadata } from "next"
 import { Inter } from "next/font/google"
 import "./globals.css"
-import { ThemeProvider } from "@/components/theme-provider"
 import NextTopLoader from "nextjs-toploader"
 import StoreProvider from "@/redux/provider"
 import { ToastContainer } from "react-toastify"
@@ -21,19 +20,17 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en" suppressHydrationWarning>
+    <html lang="en" className="light" suppressHydrationWarning>
       <head>
         <link rel="icon" href="/logo.jpg" />
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg==" crossOrigin="anonymous" referrerPolicy="no-referrer" />
       </head>
-      <body className={inter.className}>
+      <body className={inter.className} suppressHydrationWarning>
         <NextTopLoader />
         <ToastContainer position="top-right" autoClose={3000} />
 
         <StoreProvider>
-          <ThemeProvider attribute="class" defaultTheme="light" enableSystem>
             <ConditionalLayout>{children}</ConditionalLayout>
-          </ThemeProvider>
         </StoreProvider>
       </body>
     </html>
