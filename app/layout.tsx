1fb928c2024e51b81c7428c17d609558e488cbeb
UbongPr7 <ubongpr7@gmail.com>
2025-05-20 13:01:58 +0100

    styles

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,14 +23,14 @@ export default function RootLayout({
     <html lang="en" className="light" suppressHydrationWarning>
       <head>
         <link rel="icon" href="/logo.jpg" />
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg==" crossOrigin="anonymous" referrerPolicy="no-referrer" />
+        {/* Font Awesome is now imported in globals.css */}
       </head>
       <body className={inter.className} suppressHydrationWarning>
         <NextTopLoader />
         <ToastContainer position="top-right" autoClose={3000} />
 
         <StoreProvider>
-            <ConditionalLayout>{children}</ConditionalLayout>
+          <ConditionalLayout>{children}</ConditionalLayout>
         </StoreProvider>
       </body>
     </html>
